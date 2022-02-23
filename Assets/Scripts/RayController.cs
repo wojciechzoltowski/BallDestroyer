08862e1a0de8a32a3f6d586a8439c6214ab5be5e
Wojciech Żółtowski <zolwoj@wp.pl>
2022-02-23 21:19:00 +0100

    Destroying balls

---
+++ Assets/Scripts/RayController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class RayController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
